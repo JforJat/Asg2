0854b1bd0191d75064213e0b0c29cc64af0f3d38
JforJat <0199063@student.uow.edu.my>
2024-08-01 17:42:14 +0800

    Expense View has been implemented, start

--- app/src/main/java/com/example/asg2/ViewExpenseActivity.kt
+++ app/src/main/java/com/example/asg2/ViewExpenseActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ViewExpenseActivity: AppCompatActivity() {
-    //private val repository: ExpenseRepository
-    //val allExpenses: LiveData<List<Expense_Entity>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_expense)
+
+
 
     }
 
